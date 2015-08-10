77f51f9a3e6bd43fc4a2d102e481d77da35c6420
Ajay Arora <ajarora@microsoft.com>
2015-08-10 00:09:52 -0700

    Checkin Maven code

---
+++ my-app/src/test/java/com/mycompany/app/MyTest.java
@@ -0,0 +1,23 @@
+package com.mycompany.app;
+
+import junit.framework.Assert;
+import java.lang.annotation.Annotation;
+
+public class MyTest {
+
+/*	public void HelloDude() {
+ 
+		Assert.assertEquals("Hello World 2", "Hello World 2");
+ 
+	}*/
+
+	public void testApp3(){
+ 		Assert.assertEquals(App.add(2,3), 5);
+	}
+
+
+	public void testApp4(){
+ 		Assert.assertEquals(App2.sub(2,3), -1);
+	}
+ 
+}
