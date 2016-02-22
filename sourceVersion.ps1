8ffca26a77bd37a4d988c0a33dabf75c076d2731
Ajay Arora <ajarora@microsoft.com>
2016-02-21 20:44:03 -0800

    Update sourceVersion.ps1

--- sourceVersion.ps1
+++ sourceVersion.ps1
@@ -1,3 +1,3 @@
-Write-Host "1: $args[0]"
-Write-Output "2: $args[0]"
-Write-Error "3: $args[0]"
+Write-Host "1: ${env:BUILD_SOURCEVERSION}"
+Write-Output "2: ${env:BUILD_SOURCEVERSION}"
+Write-Error "3: ${env:BUILD_SOURCEVERSION}"
